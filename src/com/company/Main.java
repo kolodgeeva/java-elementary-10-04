483a4e218e8f870f9fcc8fac090ad08d34384160
mkolodzheieva <mkolodzheieva@intellias.com>
2021-04-10 16:15:58 +0300

    Add homework 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,13 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    // write your code here
+    System.out.println("ffffffffffffffffffffffffffffffffffffffffffffff"
+        + "fffffffffffffffffffffffffffffffffffff");
+  }
+
+  public static void mainMethod(String testString) {
+
+  }
 }
